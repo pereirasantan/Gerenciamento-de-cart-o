ddb60c129b1bd9885369b5ba8e3a3ee8d3a6410c
jhonbest170@gmail.com <jhonbest170@gmail.com>
2024-03-25 18:32:49 -0300

    caryao virtual part-2

--- conf.js
+++ conf.js
@@ -1,42 +1,24 @@
+const formulario = document.querySelector('#formulario')
+const check = document.querySelector('#check')
+
 var cardNumber=document.querySelector('#card-number')
 var cardName=document.querySelector('#card-name')
-var mmyy=document.querySelector('#mmyy')
-
-var yy=document.querySelector('#yy')
-
-var cvc=document.querySelector('#dig')
 
 var btnConfirm=document.querySelector('button')
-const verify=[...document.querySelectorAll('.verify')]
 
-var inputs=[...document.querySelector('input')]
+const verify=[...document.querySelectorAll('.verify')]
 
 btnConfirm.addEventListener('click',(e)=>{
   e.preventDefault()
-  verify.map((e,i)=>{
-    inputs.map((e,i)=>{
-      if(e.value==" "){
-        if (document.querySelector('#name').value == "Fabian holder") {
-          e.innerText = "correct"
-          e.style.color = "green"
-          if (document.querySelector('#num').value == 000) {
-            e.innerText = "correct"
-            e.style.color = "green"
-            if (document.querySelector('#mm').value == 09) {
-              e.innerText = "correct"
-              e.style.color = "green"
-              if (document.querySelector('#yy').value == 07) {
-                e.innerText = "correct"
-                e.style.color = "green"
-                if (document.querySelector('#dig').value == 800) {
-                  e.innerText = "correct"
-                  e.style.color = "green"
-                }
-              }
-            }
-          }
-        }
-      }
-    })
+  verify.map((e)=>{
+    if (document.querySelector('#name').value == "Fabian holder") {
+      e.style.display = "block"
+      e.innerText = "correct"
+      e.style.color = "green"
+    }
   })
+  setTimeout(()=>{
+    formulario.style.display = "none"
+    check.style.display = "flex"
+  },600)
 })
